29a56dde9e36cbf815fe5e410798c859d8e0e3c8
jameschen <chen1226@go.byuh.edu>
2023-01-26 10:57:26 -1000

    update

---
+++ app/src/main/java/com/example/alaska_x_aceh_x_byuh_androidapp/MainSplashScreen.java
@@ -0,0 +1,30 @@
+package com.example.alaska_x_aceh_x_byuh_androidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainSplashScreen extends AppCompatActivity {
+
+    private Handler mHandler;
+    private Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Intent tent = new Intent(MainSplashScreen.this, MainActivity.class);
+                startActivity(tent);
+                finish();
+            }
+        };
+        mHandler.postDelayed(mRunnable, 2000);
+    }
+}
