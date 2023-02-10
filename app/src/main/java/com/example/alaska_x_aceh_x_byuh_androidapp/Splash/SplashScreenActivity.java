ab3344166de362c55d8ca8a26f27c655bc2afd08
jameschen <chen1226@go.byuh.edu>
2023-02-09 19:24:56 -1000

    Update

---
+++ app/src/main/java/com/example/alaska_x_aceh_x_byuh_androidapp/Splash/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.alaska_x_aceh_x_byuh_androidapp.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.alaska_x_aceh_x_byuh_androidapp.Home.MainActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+}
