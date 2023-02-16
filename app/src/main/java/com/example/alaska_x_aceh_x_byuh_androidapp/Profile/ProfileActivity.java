3c25c7b49ee5a454aa54b8a3c30dd09103bc9342
Chih-Chun-Chen <chen1226@go.byuh.edu>
2023-02-16 10:12:05 -1000

    Update Color

--- app/src/main/java/com/example/alaska_x_aceh_x_byuh_androidapp/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/alaska_x_aceh_x_byuh_androidapp/Profile/ProfileActivity.java
@@ -12,7 +12,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.fragment_profile);
         ProfileFragment fragment = new ProfileFragment();
 
         getSupportFragmentManager().beginTransaction()
